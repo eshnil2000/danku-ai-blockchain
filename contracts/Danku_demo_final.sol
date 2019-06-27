be25ada034140822f38616c3e9a686c1e9f401da
Nick <eshnil2000@gmail.com>
2019-06-27 13:45:34 +0530

    Update Danku_demo_final.sol

--- contracts/Danku_demo_final.sol
+++ contracts/Danku_demo_final.sol
@@ -2,8 +2,8 @@ pragma solidity ^0.4.19;
 // Danku contract version 0.0.1
 // Data points are x, y, and z
 
-contract Danku_demo {
-  function Danku_demo() public {
+contract Danku_demo_final {
+  function Danku_demo_final() public {
     // Neural Network Structure:
     //
     // (required) input layer x number of neurons
